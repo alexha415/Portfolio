6b9262c2084bbfd8bee8bb6e87b36b700500fae0
Alex Ha <alexha415@gmail.com>
2019-01-11 02:38:24 -0800

    disclaimer not centered

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,12 +5,16 @@ import './Header.css';
 export default class Header extends Component {
   render(){
     return(
-      <Row style={{display:'flex', justifyContent:'center'}}>
+      <Col>
+      <Row style ={{display:'flex', justifyContent:'center'}}>
         <h1 id = 'disclaimer'> THIS WEBSITE IS A WORK IN PROGRESS</h1>
+      </Row>
+      <Row>
         <Col id = "header">
           <Navigation/>
         </Col>
       </Row>
+      </Col>
     );
   }
 }
